7448c2584e5c37d41ea675f61e97fa9beacb521d
Willi Kampe <willi@commerce-connector.de>
2016-09-22 17:20:20 +0200

    new initial state

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,73 @@
-const gulp = require('gulp');
-const HubRegistry = require('gulp-hub');
-const browserSync = require('browser-sync');
+/* Needed gulp config */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var neat = require('node-neat');
+var reload = browserSync.reload;
 
-const conf = require('./conf/gulp.conf');
 
-// Load some files into the registry
-const hub = new HubRegistry([conf.path.tasks('*.js')]);
+/* Scripts task */
+gulp.task('scripts', function() {
+  return gulp.src([
+    /* Add your JS files here, they will be combined in this order */
+    'js/vendor/jquery-3.0.0.min.js',
+    'js/app.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
 
-// Tell gulp to use the tasks just loaded
-gulp.registry(hub);
+/* Sass task */
+gulp.task('sass', function () {
+    gulp.src('./sass/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        includePaths: ['scss'].concat(neat)
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
+    /* Reload the browser CSS after every change */
+    .pipe(reload({stream:true}));
+});
 
-gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
-gulp.task('test', gulp.series('karma:single-run'));
-gulp.task('test:auto', gulp.series('karma:auto-run'));
-gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
-gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
-gulp.task('default', gulp.series('clean', 'build'));
-gulp.task('watch', watch);
+/* Reload task */
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
 
-function reloadBrowserSync(cb) {
-  browserSync.reload();
-  cb();
-}
+/* Prepare Browser-sync for localhost */
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/*.css', 'js/*.js'], {
+        /*
+        I like to use a vhost, WAMP guide: https://www.kristengrote.com/blog/articles/how-to-set-up-virtual-hosts-using-wamp, XAMP guide: http://sawmac.com/xampp/virtualhosts/
+        */
+        //proxy: 'your_dev_site.url'
+        /* For a static server you would use this: */
 
-function watch(done) {
-  gulp.watch(conf.path.src('app/**/*.html'), reloadBrowserSync);
-  done();
-}
+        server: {
+            baseDir: './'
+        }
+
+    });
+});
+
+/* Watch scss, js and html files, doing different things with each. */
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    /* Watch scss, run the sass task on change. */
+    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass'])
+    /* Watch app.js file, run the scripts task on change. */
+    gulp.watch(['js/app.js'], ['scripts'])
+    /* Watch .html files, run the bs-reload task on change. */
+    gulp.watch(['*.html'], ['bs-reload']);
+});
